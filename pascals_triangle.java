d810c7eba29ae285629ea71f2bdbe5ae77b92c19
Aditya Nalluri <adityanalluri9@gmail.com>
2018-01-23 18:37:11 -0500

    Create pascals_triangle.java

---
+++ pascals_triangle.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+	List<Integer> list = new ArrayList<Integer>();
+	if (rowIndex < 0)
+		return list;
+    
+	for (int i = 0; i < rowIndex + 1; i++) {
+		list.add(0,1);
+        System.out.println(list);
+		for (int j = 1; j < list.size() - 1; j++) {
+			list.set(j, list.get(j) + list.get(j + 1));
+		}
+        System.out.println(list);
+	}
+	return list;
+}
+}
